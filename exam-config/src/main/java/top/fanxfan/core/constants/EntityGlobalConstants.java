3c0877a6cd0e425eb88d19418bf8a63248565c67
fanxfan <fanxfan@aliyun.com>
2024-03-22 14:49:12 +0800

    fanxfan: 重新分层 admin:仅入口类，配置文件 config：基础和核心配置（后续可能添加common包） entity-*: 仓库相关 server：控制器相关 service：服务相关

---
+++ exam-config/src/main/java/top/fanxfan/core/constants/EntityGlobalConstants.java
@@ -0,0 +1,34 @@
+package top.fanxfan.core.constants;
+
+/**
+ * @author fanxfan
+ */
+public class EntityGlobalConstants {
+
+    private EntityGlobalConstants(){}
+
+    /**
+     * 通用前缀
+     */
+    public static final String PREFIX = "fanxfan_";
+
+    /**
+     * 用户实体名称
+     */
+    public static final String USER_ENTITY_NAME = PREFIX + "user";
+
+    /**
+     * 试题实体名称
+     */
+    public static final String QUESTION_ENTITY_NAME = PREFIX + "question";
+
+    /**
+     * 试题分类实体名称
+     */
+    public static final String QUESTION_CATALOG_ENTITY_NAME = PREFIX + "question_catalog";
+
+    /**
+     * 试题选项实体名称
+     */
+    public static final String QUESTION_OPTION_ENTITY_NAME = PREFIX + "question_option";
+}
