5fe2831a83de6ee2ca53723cb74dc638de9ff365
fanxfan <fanxfan@aliyun.com>
2024-03-26 17:20:22 +0800

    fanxfan: 相关字段提取

---
+++ exam-config/src/main/java/top/fanxfan/core/constants/FieldGlobalConstants.java
@@ -0,0 +1,31 @@
+package top.fanxfan.core.constants;
+
+/**
+ * @author fanxfan
+ */
+public class FieldGlobalConstants {
+
+    private FieldGlobalConstants() {
+
+    }
+
+    /**
+     * 用户ID
+     */
+    public static final String USER_ID = "userId";
+
+    /**
+     * 父级id字段
+     */
+    public static final String PARENT_ID_FIELD = "parentId";
+
+    /**
+     * 问题ID
+     */
+    public static final String QUESTION_ID_FIELD = "questionId";
+
+    /**
+     * 试题分类ID
+     */
+    public static final String QUESTION_CATALOG_ID_FIELD = "questionCatalogId";
+}
---
+++ exam-service/src/main/java/top/fanxfan/exam/contants/ExamErrorConstants.java
@@ -0,0 +1,13 @@
+package top.fanxfan.exam.contants;
+
+/**
+ * @author fanxfan
+ */
+@SuppressWarnings("all")
+public interface ExamErrorConstants {
+
+    /**
+     * 试题不存在
+     */
+    public static final String QUESTION_NOT_FOUND = "试题不存在";
+}
